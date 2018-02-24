c6addf23b2ab621d885e394202faeea8d09be1a1
Fabian PETRAS <fpetras@e1r7p4.42.fr>
2018-02-24 16:23:08 +0100

    Multi-column display, automatic repositioning upon window resizing

--- display.c
+++ display.c
@@ -6,33 +6,63 @@
 /*   By: fpetras <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/23 09:45:25 by fpetras           #+#    #+#             */
-/*   Updated: 2018/02/23 11:16:19 by fpetras          ###   ########.fr       */
+/*   Updated: 2018/02/24 14:37:20 by fpetras          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_select.h"
 
-static void	ft_color(char *arg)
+static int	ft_max_arglen(void)
 {
-	(void)arg;
-	ft_dprintf(1, "\033[0m");
+	int i;
+	int max;
+
+	i = 0;
+	max = 0;
+	while (g_sel.args[i])
+	{
+		if ((int)ft_strlen(g_sel.args[i]) > max)
+			max = (int)ft_strlen(g_sel.args[i]);
+		i++;
+	}
+	return (max);
 }
+
+static void	ft_print_arg_single_column(char *arg, int index)
+{
+	if (g_sel.cursor == index)
+		tputs(tgetstr("us", NULL), 1, ft_putc);
+	if (g_sel.selected[index] == SELECTED)
+		tputs(tgetstr("mr", NULL), 1, ft_putc);
+	ft_dprintf(g_sel.fd, "%s\n", arg);
+	if (g_sel.color)
+		ft_color(index);
+	else
+		ft_dprintf(g_sel.fd, "\033[0m");
+}
+
+/*
+** Display all arguments in a single column if the window's height is sufficient
+** Display only if the window's width is larger than any argument
+*/
 
 void		ft_display(void)
 {
 	int i;
 
 	i = 0;
-	while (i < (int)ft_tablen(g_sel.args))
+	if ((int)ft_tablen(g_sel.args) < g_sel.rows && ft_max_arglen() < g_sel.cols)
 	{
-		if (g_sel.cursor == i)
-			tputs(tgetstr("us", NULL), 1, ft_putc);
-		if (g_sel.selected[i] == SELECTED)
-			tputs(tgetstr("mr", NULL), 1, ft_putc);
-		ft_dprintf(1, "%s\n", g_sel.args[i]);
-		ft_color(g_sel.args[i]);
-		i++;
+		while (i < (int)ft_tablen(g_sel.args))
+		{
+			ft_print_arg_single_column(g_sel.args[i], i);
+			i++;
+		}
 	}
+	else if (ft_max_arglen() > g_sel.cols)
+		ft_dprintf(g_sel.fd, "Window\nsize\nis\ntoo\nsmall\n🖼");
+	else
+		ft_display_multi_column();
 }
 
 void		ft_display_selection(void)
---
+++ display2.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   display2.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpetras <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/24 13:52:52 by fpetras           #+#    #+#             */
+/*   Updated: 2018/02/24 14:33:37 by fpetras          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_select.h"
+
+static void	ft_print_arg_multi_column(char *arg, int index)
+{
+	if (g_sel.cursor == index)
+		tputs(tgetstr("us", NULL), 1, ft_putc);
+	if (g_sel.selected[index] == SELECTED)
+		tputs(tgetstr("mr", NULL), 1, ft_putc);
+	ft_dprintf(g_sel.fd, "%s", arg);
+	if (g_sel.color)
+		ft_color(index);
+	else
+		ft_dprintf(g_sel.fd, "\033[0m");
+}
+
+/*
+** Storing of the total length of arguments printed in the current line
+** Checking if the next argument still fits in the current line
+** If not, go the next line
+*/
+
+static void	ft_display_multi_column2(void)
+{
+	int i;
+	int line;
+
+	i = 1;
+	line = (int)ft_strlen(g_sel.args[0]);
+	ft_print_arg_multi_column(g_sel.args[0], 0);
+	while (g_sel.args[i])
+	{
+		if (line + (int)ft_strlen(g_sel.args[i]) < g_sel.cols)
+		{
+			ft_dprintf(g_sel.fd, " ");
+			ft_print_arg_multi_column(g_sel.args[i], i);
+		}
+		else
+		{
+			ft_dprintf(g_sel.fd, "\n");
+			ft_print_arg_multi_column(g_sel.args[i], i);
+			line = 0;
+		}
+		line += (int)ft_strlen(g_sel.args[i]);
+		line++;
+		i++;
+	}
+}
+
+/*
+** Calculation to verify that the window's size is large enough to fit all args
+*/
+
+void		ft_display_multi_column(void)
+{
+	int i;
+	int len;
+
+	i = 0;
+	len = 0;
+	while (g_sel.args[i])
+	{
+		len += (int)ft_strlen(g_sel.args[i]);
+		len++;
+		i++;
+	}
+	if (len > (g_sel.cols * g_sel.rows) * 0.75)
+		ft_dprintf(g_sel.fd, "Window\nsize\nis\ntoo\nsmall\n🖼");
+	else
+		ft_display_multi_column2();
+}
